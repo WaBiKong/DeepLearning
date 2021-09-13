3b7669d13ef420ad1dcdce795ec4ea7aea625330
WaBiKong <ricardo2080355415@gmail.com>
2021-09-13 16:08:45 +0800

    updata

---
+++ 2 预备知识/2.3 线性代数/2.3.3 向量矩阵乘积.py
@@ -0,0 +1,24 @@
+# 矩阵向量乘积
+import torch
+
+# 向量-向量相乘（点积）
+x = torch.arange(4, dtype=torch.float32)
+y = torch.arange(4, dtype=torch.float32)
+print('x:', x)
+print('y:', y)
+print('向量-向量点积torch.dot(x, y):', torch.dot(x, y))  # torch.dot()计算向量乘向量的点积
+
+# 矩阵-向量相乘（向量积）
+A = torch.arange(20, dtype=torch.float32).reshape(5, 4)
+print('A:\n', A)
+print('x:', x)
+print('torch.mv(A, x):', torch.mv(A, x))  # torch.mv()计算矩阵乘向量
+
+# 矩阵-矩阵相乘（向量积）
+B = torch.ones(4, 3)  # 4*3维 全1 矩阵
+print('B:\n', B)
+print('torch.mm(A, B):\n', torch.mm(A, B))  # torch.mm()计算矩阵乘矩阵
+
+
+
+
